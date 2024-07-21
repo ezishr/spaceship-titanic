6e0e31eeb8ab8d8f03bcf2b4964a91932ea74122
agalvezv <43121059+agalvezv@users.noreply.github.com>
2024-07-20 21:56:37 -0400

    Adding py file

---
+++ st_proj.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+
+from imblearn.over_sampling import RandomOverSampler
+
+import copy
+
+import seaborn as sns
