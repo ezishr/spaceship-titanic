69b6d9d28530ea02d8b6bd3e69753fb0b6ed411f
agalvezv <43121059+agalvezv@users.noreply.github.com>
2024-07-20 22:24:26 -0400

    Updated py

--- st_proj.py
+++ st_proj.py
@@ -11,3 +11,34 @@ from imblearn.over_sampling import RandomOverSampler
 import copy
 
 import seaborn as sns
+
+
+df = pd.read_csv("train.csv")
+dataset_cols = [
+  "Passenger",
+  "CryoSleep",
+  "HomePlanet",
+  "Cabin",
+  "Destination",
+  "Age",
+  "VIP",
+  "RoomService",
+  "FoodCourt",
+  "ShoppingMall",
+  "Spa",
+  "VRDeck",
+  "Transported"
+]
+
+df.cols = dataset_cols
+df.head()
+
+
+df = df.drop(["PassengerId"], axis = 1)
+df = df.drop(["Name"], axis = 1)
+
+
+print("Unique Home Planets:", df['HomePlanet'].unique())
+planet_map = {"Europa": 1, "Earth": 2, "Mars": 3, np.nan: -1}  # Include np.nan mapping
+df["HomePlanet"] = df["HomePlanet"].map(planet_map).fillna(-1).astype(int)  # home planet to int
+df.head()
